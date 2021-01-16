19f9727775ddcf1a3e6b30bc107c9f9e5af3c321
kerenomero <kerenome19@gmail.com>
2021-01-16 20:20:58 +0200

    show saved training and start the the chosen training

--- trainer/src/Components/LikedTrains.js
+++ trainer/src/Components/LikedTrains.js
@@ -1,36 +1,55 @@
 import React from 'react';
 import "./CssComponents/LikedTrains.css";
-import {Button} from 'semantic-ui-react'
+import {Icon, Button} from 'semantic-ui-react'
+import { Container, Row, Col } from "shards-react";
+import {NavLink} from 'react-router-dom';
+
 class LikedTrains extends React.Component {
     constructor(props) {
         super(props);
          this.state = {
-                savedTrainingFromDB : [
-                 {name: "BirdDog", time: 5000},
-                        {name: "Superman", time: 10000},
-                        {name: "CatCow", time: 40000},
-                        {name: "FinalTraining", time: 5000}
-                        ],
-                 chosenSavedTrainingName : "",
-                 chosenSavedTrainingNameExercises : []
-         };
-     this.updateChosenSavedTraining = this.updateChosenSavedTraining.bind(this);
-     this.getTrainingExercisesFromDB = this.getTrainingExercisesFromDB.bind(this);
-     }
+             savedTrainingFromDB: [],
+             chosenLikedTrainingByName: [],
+             isUpdatedChosenSavedTraining: false
+         }
+      this.updateChosenSavedTraining = this.updateChosenSavedTraining.bind(this);
+      this.getAllTraining = this.getAllTraining.bind(this);
+   }
 
- getTrainingExercisesFromDB = (trainingName) => {
-  return [ {name: "BirdDog", time: 5000},
-          {name: "Superman", time: 10000}];
- }
+  async getAllTraining(){
+         try {
+             const response = await fetch('http://localhost:5000/api/allTraining');
+             const data = await response.json();
+             console.log(data);
+             this.setState({
+                     savedTrainingFromDB: data
+             })
+         }
+         catch(error){
+             console.log(error);
+         }
+  }
 
- updateChosenSavedTraining = (trainingName) => {
-    var savedTrainingExercisesArray = this.getTrainingExercisesFromDB(trainingName);
+ componentDidMount() {
+   this.getAllTraining();
+}
+
+updateChosenSavedTraining = (trainingName) => {
     this.setState({
-      chosenSavedTrainingName: trainingName,
-      chosenSavedTrainingNameExercises: savedTrainingExercisesArray
-    })
-  };
-
+          isUpdatedChosenSavedTraining: false
+    });
+    this.state.savedTrainingFromDB.map((item, index) => {
+            if(item.name === trainingName) {
+                    console.log(item.exerciseList)
+                    this.setState({
+                          chosenLikedTrainingByName: item.exerciseList
+                        });
+                }
+        return null })
+         this.setState({
+             isUpdatedChosenSavedTraining: true
+       });
+   }
 
 render(){
        return (
@@ -41,22 +60,40 @@ render(){
                                 <div className="myLeftCtn">
                                    <h3> Choose your favorite training! </h3>
                                    <Button.Group basic vertical className="trainingNameGroup">
-                                                 {this.state.savedTrainingFromDB.map((training)  => (
-                                                    <Button basic color="blue"
-                                                    onClick={() =>  this.updateChosenSavedTraining(training.name)
-                                                     } >{training.name}</Button>
-                                                 ))}
+                                       {this.state.savedTrainingFromDB.map((training, index)  => (
+                                           <div>
+                                           <li key={index}/>
+                                           <Button basic color="blue"
+                                           onClick={() => this.updateChosenSavedTraining(training.name)}>
+                                           {training.name}
+                                           </Button>
+                                           </div>
+                                           ))}
                                   </Button.Group>
                                  </div>
                             </div>
                             <div className="col-md-6">
                                 <div className="myRightCtn">
-                                    {  (this.state.chosenSavedTrainingFromDB !== "") ?
-                                                   <div>
-                                                   <h3> {this.state.chosenSavedTrainingName} </h3>
-                                                   </div>
-                                           : ""
-                                       }
+                                          {(this.state.isUpdatedChosenSavedTraining === true) ?
+                                                (<Container>
+                                                    <Row className="py-4">
+                                                         <h1 className="text-white">Training List</h1>
+                                                    </Row>
+                                                    <Row className="mb-2">
+                                                        {this.state.chosenLikedTrainingByName.map((exercise) =>
+                                                           (exercise.name !== "Rest" ? (
+                                                            <Col xs="11">
+                                                            <h3 className="text-white">{exercise.name}</h3> Duration per set: {exercise.time} seconds
+                                                            </Col> ) : null ))}
+                                                            </Row>
+                                                    <NavLink to = {{
+                                                                pathname: `/Timer/`,
+                                                                props: { exercisesArray: this.state.chosenLikedTrainingByName }
+                                                                      }}>
+                                                          <Button icon labelPosition="right" color="blue">START TRAINING<Icon name="angle double right" /></Button>
+                                                     </NavLink>
+                                                    </Container>)
+                                            : null}
                                 </div>
                             </div>
                         </div>
