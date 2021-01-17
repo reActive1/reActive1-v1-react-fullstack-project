993e1ecf374e90f0156c25d81945f2c6c4a5eec6
steinshi <steinshi@post.bgu.ac.il>
2021-01-17 21:27:55 +0200

    wip

--- trainer/src/Components/ExerciseForm.js
+++ trainer/src/Components/ExerciseForm.js
@@ -11,7 +11,7 @@ import axios from "axios";
 
 class ExerciseForm extends React.Component {
   constructor(props) {
-    // super(props);
+    super(props);
 
     // var ex = Object.keys(Exercises).map((key) => {
     //   var obj = {};
